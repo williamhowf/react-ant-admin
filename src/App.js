5e84e1df2c700efcb4da79dceee05171be6b3066
William Ho Weng Fei (D) <williamhowf@gmail.com>
2020-11-14 00:54:57 +0800

    update from git desktop
    git desktop update

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function myAlert(num) {
 
 // public function when import App.js
 function getRandomString(length) {
+  //random function
   var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
   var result = '';
   for ( var i = 0; i < length; i++ ) {
